389ee7b6f774113d408f6abfaa8b07d775bd28f1
Mix <32300164+mnixry@users.noreply.github.com>
2025-08-07 01:08:52 +0800

    update flake lock and remove unused inputs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware/master";
