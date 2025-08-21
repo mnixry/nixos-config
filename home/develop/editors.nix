6e489e62fa9b805d1c06e4d41f61a3821c2c0d16
Mix <32300164+mnixry@users.noreply.github.com>
2025-08-21 15:06:05 +0800

    refactor editors.nix to override code-cursor with system built Electron

--- home/develop/editors.nix
+++ home/develop/editors.nix
@@ -1,5 +1,20 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  home.packages = with pkgs; [ code-cursor ];
+  home.packages = with pkgs; [
+    (code-cursor.overrideAttrs (_: {
+      # force cursor to use system built Electron
+      postInstall =
+        let
+          electron = lib.getExe pkgs.electron;
+        in
+        ''
+          sed -i \
+            -e "/ELECTRON=/iVSCODE_PATH='$out/lib/cursor'" \
+            -e 's|^ELECTRON=.*|ELECTRON=${electron}|' \
+            -e 's|ELECTRON_RUN_AS_NODE=1 "$ELECTRON" "$CLI" "$@"|ELECTRON_RUN_AS_NODE=1 "$ELECTRON" "$CLI" --app "$VSCODE_PATH/resources/app" "$@"|' \
+            "$out/bin/cursor"
+        '';
+    }))
+  ];
   programs.vscode.enable = true;
 }
