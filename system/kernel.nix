4a99dbdfa659c4b78b7e055008c4694ea1b1a478
Mix <32300164+mnixry@users.noreply.github.com>
2025-10-16 12:45:16 +0800

    move from native to x86-64-v3 march kernel

--- system/kernel.nix
+++ system/kernel.nix
@@ -1,39 +1,40 @@
 { pkgs, inputs, ... }:
 let
-  linuxPackages = pkgs.linuxPackages_cachyos.override (super: {
-    stdenv = pkgs.impureUseNativeOptimizations super.stdenv;
-  });
-  kernelPackages = (linuxPackages.cachyOverride { mArch = "NATIVE"; }).extend (
-    lpself: lpsuper: { inherit (pkgs.linuxPackages_cachyos-gcc) evdi nvidiaPackages; }
-  );
+  kernelPackages =
+    with pkgs;
+    (linuxPackages_cachyos.cachyOverride { mArch = "GENERIC_V3"; }).extend (
+      lpself: lpsuper: { inherit (linuxPackages_cachyos-gcc) evdi; }
+    );
 in
 {
   imports = [ inputs.chaotic.nixosModules.default ];
 
-  boot.kernelPackages = kernelPackages;
-  boot.kernel.sysctl =
-    let
-      MB = 1024 * 1024;
-      mkKB = x: toString (x * 1024);
-      mkMB = x: toString (x * MB);
-    in
-    {
-      "net.core.default_qdisc" = "fq";
-      "net.ipv4.tcp_congestion_control" = "bbr";
-      "net.core.rmem_max" = 64 * MB;
-      "net.core.wmem_max" = 64 * MB;
-      "net.ipv4.tcp_rmem" = "${mkKB 8} ${mkKB 256} ${mkMB 32}";
-      "net.ipv4.tcp_wmem" = "${mkKB 4} ${mkKB 128} ${mkMB 32}";
-      "net.ipv4.tcp_adv_win_scale" = "-2";
+  boot = {
+    inherit kernelPackages;
+    kernel.sysctl =
+      let
+        MB = 1024 * 1024;
+        mkKB = x: toString (x * 1024);
+        mkMB = x: toString (x * MB);
+      in
+      {
+        "net.core.default_qdisc" = "fq";
+        "net.ipv4.tcp_congestion_control" = "bbr";
+        "net.core.rmem_max" = 64 * MB;
+        "net.core.wmem_max" = 64 * MB;
+        "net.ipv4.tcp_rmem" = "${mkKB 8} ${mkKB 256} ${mkMB 32}";
+        "net.ipv4.tcp_wmem" = "${mkKB 4} ${mkKB 128} ${mkMB 32}";
+        "net.ipv4.tcp_adv_win_scale" = "-2";
+      };
+    initrd = {
+      compressor = "zstd";
+      compressorArgs = [
+        "--long"
+        "--ultra"
+        "-22"
+        "-T0"
+      ];
     };
-  boot.initrd = {
-    compressor = "zstd";
-    compressorArgs = [
-      "--long"
-      "--ultra"
-      "-22"
-      "-T0"
-    ];
   };
 
   services.scx = {
