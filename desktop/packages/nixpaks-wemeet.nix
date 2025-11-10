2ccc968fc190e44d02ef3517b0d82ccbee0b2112
Mix <32300164+mnixry@users.noreply.github.com>
2025-11-10 10:50:07 +0800

    remove override of wemeet

--- desktop/packages/nixpaks-wemeet.nix
+++ desktop/packages/nixpaks-wemeet.nix
@@ -1,7 +1,6 @@
 {
   lib,
   wemeet,
-  fetchurl,
   buildEnv,
   mkAppWrapper,
   mkNixPak,
@@ -14,17 +13,7 @@ let
       { ... }:
       {
         imports = [ ./nixpaks-common.nix ];
-        app.package = mkAppWrapper (wemeet.overrideAttrs (_: {
-          version = "3.26.10.400";
-          src = fetchurl {
-            url = "https://updatecdn.meeting.qq.com/cos/9cfd93b10ee81b2fc3ad26357f27ed13/TencentMeeting_0300000000_3.26.10.400_x86_64_default.publish.deb";
-            hash = "sha256-7gN40mkAD/0/k0E+bBNfiMcY+YtIaLWycFoI+hhrjgc=";
-          };
-          postInstall = ''
-            # cmp rdi, 0 -> cmp r12, 0, at co_jump_to_link to address coroutine context resume issue
-            echo -ne '\x49\x83\xfc\x00' | dd of=$out/app/wemeet/lib/libwemeet_base.so bs=1 seek=$((0x94c833)) conv=notrunc
-          '';
-        })) { binPath = "bin/wemeet-xwayland"; };
+        app.package = mkAppWrapper wemeet { binPath = "bin/wemeet-xwayland"; };
         flatpak = { inherit appId; };
         bubblewrap = {
           sockets = {
