a469a712ba722133b6a08db576b4108b81dd7263
Mix <32300164+mnixry@users.noreply.github.com>
2026-07-12 00:29:09 +0800

    simplify python package configuration
    - Simplified the package wrapping logic for Python packages.
    - Updated the home packages list to use a more concise syntax.
    - Enabled the ruff program in the configuration.

--- home/common/develop/python.nix
+++ home/common/develop/python.nix
@@ -1,67 +1,36 @@
-{ pkgs, lib, ... }:
-let
-  mkNixLDwrappedPackage =
-    package:
-    if pkgs.stdenv.isLinux then
-      let
-        binOverride = pkgs.symlinkJoin {
-          name = "nix-ld-override-${package.name}";
-          paths =
-            map
-              (
-                name:
-                pkgs.writeShellScriptBin name ''
-                  export LD_LIBRARY_PATH="''${NIX_LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:''${LD_LIBRARY_PATH}}"
-                  exec '${lib.getExe' package name}' "$@"
-                ''
-              )
-              (
-                builtins.attrNames (
-                  lib.attrsets.filterAttrs (path: _type: _type != "directory") (builtins.readDir "${package}/bin/")
-                )
-              );
-        };
-      in
-      pkgs.buildEnv {
-        inherit (package) name meta passthru;
-        paths = [
-          (lib.hiPrio binOverride)
-          package
-        ];
-      }
-    else
-      package;
-
-  pythonPackages =
-    ps: with ps; [
-      pip
-      ptpython
-      virtualenv
-
-      numpy
-      pandas
-      scipy
-      pillow
-
-      requests
-      httpx
-      rich
-
-      sympy
-      cryptography
-      pycryptodome
-      gmpy2
-
-      pwntools
-      ropper
-    ];
-in
+{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    (mkNixLDwrappedPackage (python3.withPackages pythonPackages))
-    (mkNixLDwrappedPackage pypy3)
-    ruff
+  home.packages = [
+    (pkgs.python3.withPackages (
+      ps: with ps; [
+        pip
+        ptpython
+        virtualenv
+
+        numpy
+        pandas
+        scipy
+        pillow
+
+        requests
+        httpx
+        rich
+
+        sympy
+        cryptography
+        pycryptodome
+        gmpy2
+
+        pwntools
+        ropper
+      ]
+    ))
+    pkgs.pypy3
   ];
+
+  programs.ruff = {
+    enable = true;
+  };
 
   programs.pdm = {
     enable = true;
