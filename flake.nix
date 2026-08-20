4798baeaa38ac1e4392ee640bebd4a93f4f5322b
Mix <32300164+mnixry@users.noreply.github.com>
2026-08-21 01:31:38 +0800

    switch nixos nixpkgs to nixos-unstable-small branch
    [skip ci]

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-nixos.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-nixos.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
