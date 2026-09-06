7b3662e4908fd79600223ee0f9c88a57b8db38d6
Mix <32300164+mnixry@users.noreply.github.com>
2026-09-06 11:44:54 +0800

    refactor editor package overrides and clean up obs plugins
    - Removed unnecessary Wayland overrides for JetBrains DataGrip and IntelliJ IDEA in editors.nix.
    - Simplified OBS Studio configuration by removing commented-out plugin entries in obs-studio.nix.

--- modules/home/linux/multimedia/obs-studio.nix
+++ modules/home/linux/multimedia/obs-studio.nix
@@ -1,33 +1,6 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.obs-studio = {
     enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      # screen capture
-      wlrobs
-      # obs-ndi
-      # obs-nvfbc
-      obs-teleport
-      # obs-hyperion
-      # droidcam-obs
-      obs-vkcapture
-      obs-gstreamer
-      input-overlay
-      obs-multi-rtmp
-      obs-source-clone
-      obs-shaderfilter
-      obs-source-record
-      obs-livesplit-one
-      looking-glass-obs
-      obs-vintage-filter
-      obs-command-source
-      # https://github.com/NixOS/nixpkgs/pull/556032
-      # obs-move-transition
-      obs-backgroundremoval
-      # advanced-scene-switcher
-      obs-pipewire-audio-capture
-      obs-vaapi
-      obs-3d-effect
-    ];
   };
 }
